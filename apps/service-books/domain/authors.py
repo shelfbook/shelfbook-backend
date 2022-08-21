c911b97655dcedf60e101f291a0c8d63a257922a
Vitaly <mr.peunov@gmail.com>
2022-08-21 12:52:32 +0300

    Фикс

--- apps/service-books/domain/authors.py
+++ apps/service-books/domain/authors.py
@@ -1,12 +1,19 @@
+from datetime import datetime
+
 from core.db import metadata
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, Boolean, DateTime
 
 authors = Table(
     'authors',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True, unique=True),
-    Column('first_name', String),
-    Column('last_name', String),
+    Column('first_name', String(64)),
+    Column('middle_name', String(64), nullable=True, default=None),
+    Column('last_name', String(64)),
     Column('birth_year', Integer),
-    Column('dead_year', Integer)
+    Column('dead_year', Integer),
+    Column('is_confirmed', Boolean, default=False),
+    Column('is_processed', Boolean, default=False),
+    Column('created_at', DateTime, default=datetime.now),
+    Column('updated_at', DateTime, onupdate=datetime.now)
 )
